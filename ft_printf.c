1b4f4b5386e89b7ab3c08f63a3e3c88693010dbe
David JOLY <djoly@e2r9p3.42.fr>
2016-01-26 17:28:42 +0100

    push2

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 12:40:46 by djoly             #+#    #+#             */
-/*   Updated: 2016/01/26 16:41:02 by djoly            ###   ########.fr       */
+/*   Updated: 2016/01/26 17:28:11 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,53 +15,51 @@
 
 void ft_check_token(const char *format, va_list *ap, t_t_list *t_token)
 {
-    void *a;
-    a = va_arg(*ap, void *);
-    while (t_token)
-    {
-        if (t_token->c == *format)
-            t_token->fonction(a);
-        t_token = t_token->next;
-    }
-    /*
-    if (*format == 'd')
-        ft_putnbr((int) a);
-    else if (*format == 'c')
-        ft_putchar((char) a);*/
+	void *a;
+	a = va_arg(*ap, void *);
+	while (t_token)
+	{
+		if (t_token->c == *format)
+		t_token->fonction(a);
+		t_token = t_token->next;
+	}
+	/*
+	if (*format == 'd')
+		ft_putnbr((int) a);
+	else if (*format == 'c')
+		ft_putchar((char) a);*/
 }
 int     ft_printf(const char *format, ...)
-{
-    t_t_list     *t_token;
-    va_list     ap;
-    //void    *ret;
-    //int      ret;
-    t_token = malloc(sizeof(t_t_list));
-    ft_init_list(t_token);
-    va_start(ap, format);
-    while(*format != '\0')
-    {
-        if (*format == '%')
-            {
-                if(format[1] == '%')
-                {
-                    ft_putchar('%');
-                    format++;
-                }
-                else
-                    ft_check_token(++format, &ap, t_token);
-                }
-                else
-                ft_putchar(*format);
-            format++;
-        }
-        return (0);
-
-    /*ret = va_arg(ap, void* );
-    printf("%d", (int) ret);
-    ret = va_arg(ap, void *);
-    printf("%c", (char) ret);*/
-    va_end(ap);
-    return (0);
+	{
+	t_t_list     *t_token;
+	va_list     ap;
+	//void    *ret;
+	//int      ret;
+	t_token = malloc(sizeof(t_t_list));
+	ft_init_list(t_token);
+	va_start(ap, format);
+	while(*format != '\0')
+	{
+		if (*format == '%')
+			{
+				if(format[1] == '%')
+				{
+					ft_putchar('%');
+					format++;
+				}
+				else
+				ft_check_token(++format, &ap, t_token);
+			}
+			else
+				ft_putchar(*format);
+			format++;
+	}
+	/*ret = va_arg(ap, void* );
+	printf("%d", (int) ret);
+	ret = va_arg(ap, void *);
+	printf("%c", (char) ret);*/
+	va_end(ap);
+	return (0);
 }
 
 int main ()
--- token.c
+++ token.c
@@ -6,26 +6,24 @@
 /*   By: djoly <djoly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/26 15:22:46 by djoly             #+#    #+#             */
-/*   Updated: 2016/01/26 16:44:05 by djoly            ###   ########.fr       */
+/*   Updated: 2016/01/26 17:25:39 by djoly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void ft_init_list(t_t_list *t_token)
+void	ft_init_list(t_t_list *t_token)
 {
+	t_t_list	*t2;
+	int			a;
 
-    t_t_list *t2;
-
-
-    t2 = (t_t_list*)malloc(sizeof(t_t_list));
-    t_token->c = 'c';
-    t_token->fonction = ft_putchar;
-    t_token->next = t2;
-
-    t2->c = 'd';
-    t2->fonction = ft_putnbr;
-    t2->next = NULL;
+	t2 = (t_t_list*)malloc(sizeof(t_t_list));
+	t_token->c = 'c';
+	t_token->fonction = ft_putchar;
+	t_token->next = t2;
+	t2->c = 'd';
+	t2->fonction = ft_putnbr;
+	t2->next = NULL;
 }
 /*
 ft_token(const char *format, va_list *ap)
