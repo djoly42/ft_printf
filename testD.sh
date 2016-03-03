56c1ef55556aad5cbd2a711941a1023df306b61c
Your Name <you@example.com>
2016-03-03 18:26:10 +0100

    rm  CDODf

--- testD.sh
+++
@@ -1,5 +0,0 @@
-rm ./a.out
-make fclean
-make aclean
-gcc mainD.c libftprintf.a
-./a.out # | cat -e
--- testf.sh
+++
@@ -1,5 +0,0 @@
-rm ./a.out
-make fclean
-make aclean
-gcc mainf.c libftprintf.a
-./a.out # | cat -e
